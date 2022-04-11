a521baba553f258b59e100b84725a7a2eeaa0ad1
Blake Beckstrom <dabeck88@gmail.com>
2022-04-11 12:25:14 -0400

    added lowdb starter

--- simple.js
+++ simple.js
@@ -1,23 +1,24 @@
-import { join, dirname } from 'path'
-import { Low, JSONFile } from 'lowdb'
-import { fileURLToPath } from 'url'
+import { join, dirname } from "path";
+import { Low, JSONFile } from "lowdb";
+import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Use JSON file for storage
-const file = join(__dirname, 'db.json')
-const adapter = new JSONFile(file)
-const db = new Low(adapter)
+const file = join(__dirname, "db.json");
+const adapter = new JSONFile(file);
+const db = new Low(adapter);
 
 // Read data from JSON file, this will set db.data content
-await db.read()
+await db.read();
 
 // init the data store
 db.data ||= { posts: [] };
 
 // add post
 // ----------------------------
-db.
+db.data.posts.push("hello world");
+db.data.posts[0];
 
 // count posts
 // ----------------------------
